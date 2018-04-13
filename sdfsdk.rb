5d0a0c9d404759aa0d61654dc52308c495dfbdc5
TJ Tyrrell <innovate@tjtyrrell.com>
2018-04-13 16:45:11 -0600

    18.1.1: Updated for SDF SDK

--- sdfsdk.rb
+++ sdfsdk.rb
@@ -1,14 +1,14 @@
 class Sdfsdk < Formula
   desc "NetSuite SDF SDK Tool"
-  homepage "https://system.netsuite.com/app/help/helpcenter.nl?fid=chapter_4779302061.html"
-  url "https://github.com/limebox/sdf/raw/18.1.0/Files/brew/sdfsdk-18.1.0.tar.gz"
-  sha256 "10aba426e41136d721ed2d191355e92e25726350"
+  homepage "https://system.netsuite.com/app/help/helpcenter.nl?fid=section_1489072409.html"
+  url "https://github.com/limebox/sdf/raw/18.1.1/Files/brew/sdfsdk-18.1.1.tar.gz"
+  sha256 "b7cd0f45a3b2e4858c3264d937d36ef7152c770c08e5130bcec67a6803a58240"
 
   depends_on "maven" => :build
 
   def install
-    bin.install "sdfsdk", "sdfsdk-createproject"
-    libexec.install "pom.xml", "com.netsuite.ide.core_2018.1.0.jar"
+    bin.install "sdfcli", "sdfcli-createproject"
+    libexec.install "pom.xml", "com.netsuite.ide.core_2018.1.1.jar", "axis.jar", "NetSuiteWebService.jar"
   end
 
   test do
