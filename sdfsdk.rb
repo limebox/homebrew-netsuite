03e2e11770006d932f40aabb57422ac47955c161
TJ Tyrrell <innovate@tjtyrrell.com>
2019-03-03 13:10:19 -0500

    master: Reset main installer back to 18.2.1

--- sdfsdk.rb
+++ sdfsdk.rb
@@ -1,18 +1,18 @@
-class Sdfsdk < Formula
+class SdfsdkAT1821 < Formula
   desc "NetSuite SDF SDK Tool"
   homepage "https://limebox.com/developers/sdfsdk"
-  url "https://system.netsuite.com/download/suitecloud-sdk/cli/java/19.1/sdf-cli.tar.gz"
-  sha256 "a59075bc7f1089ba86bbcef5b25ddb788f59ad87402cedcbbea167eb0ed605c0"
-  version "19.1.0"
+  url "https://github.com/limebox/sdf/raw/18.2.1/Files/brew/sdfsdk-18.2.1.tar.gz"
+  sha256 "7cc40481acb67147728b76e2c3330b27517cce90d8e03285d1aa76206d22893b"
+  version "18.2.1"
 
-  # Resolve cask dependencies
-  system "/usr/local/bin/brew", "cask", "install", "caskroom/versions/java8"
+  depends_on "maven" => :build
 
   def install
-    bin.install "sdfcli", "cli-2019.1.0.jar"
+    bin.install "sdfcli"
+    libexec.install "pom.xml", "com.netsuite.ide.core_2018.2.1.jar", "axis.jar", "NetSuiteWebService.jar", ".clicache"
   end
 
   test do
-    system "#{bin}/sdfcli"
+    system "#{bin}/sdfsdk"
   end
 end
