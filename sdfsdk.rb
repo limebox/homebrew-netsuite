7b09e7f965f89de6af2cbfe322d949c52b633bbd
christopher wallace <chrstphrwllc@gmail.com>
2021-07-20 10:41:42 -0500

    Update sdfsdk.rb
    Updated for newest version.

--- sdfsdk.rb
+++ sdfsdk.rb
@@ -2,16 +2,16 @@ class Sdfsdk < Formula
   desc "NetSuite SDF SDK Tool"
   homepage "https://limebox.com/developers/sdfsdk"
   url "https://system.netsuite.com/download/suitecloud-sdk/cli/java/latest/sdf-cli.tar.gz"
-  sha256 "53f23b13239bc8dcd97007d5ad2dae4e7c3098d1b607448545636cd5eb33840d"
-  version "20.2.0"
+  sha256 "d98b3293ec246740c7ed12a131b737c078b017ca64dfc09e2845bae2625ef445"
+  version "21.1.1"
 
   # Resolve cask dependencies
   depends_on :java => "11.0"
 
   def install
 
-        system "curl", "https://raw.githubusercontent.com/limebox/homebrew-netsuite/20.1.1/sdfsdk", "-o", "#{buildpath}/sdfsdk"
-    bin.install "sdfcli", "cli-2020.2.0.jar","sdfsdk"
+        system "curl", "https://raw.githubusercontent.com/limebox/homebrew-netsuite/21.1.1/sdfsdk", "-o", "#{buildpath}/sdfsdk"
+    bin.install "sdfcli", "cli-2021.1.1.jar","sdfsdk"
 
   end
 
